bae49690dc2022d0fca4e819816470d79d2b5de8
Kelvin Kapaya <kalvinkapu@gmail.com>
2023-07-10 09:20:22 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// // Write your code here!
+
+document.getElementById('main').remove();
+
+// Create a new header element
+const newHeader = document.createElement('h1')
+// Set the id of the new header
+newHeader.id = 'victory'
+// Set the inner text of the new header
+newHeader.innerHTML = "YOUR-NAME is the champion"
